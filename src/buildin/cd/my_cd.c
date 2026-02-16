c57b4ca0dfc8ad726f1e4f4d06704132deedd8c2
Enzo Villalba <enzo.villalba@epitech.eu>
2026-02-16 17:35:43 +0100

    sync: 2026-02-16 17:35:43

---
+++ src/buildin/cd/my_cd.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-200-LYN-2-1-minishell1-5
+** File description:
+** my_cd.c
+*/
+
+#include "my.h"
+
+void my_cd(char **cmd, shell_t *shell)
+{
+    char oldpwd[1024];
+
+    if (!getcwd(oldpwd, sizeof(oldpwd)))
+    {
+        perror("cd");
+        return;
+    }
+    return;
+}
