9b76c04e2100f152b2f176bbf4a9a5bef92402ad
Enzo Villalba <enzo.villalba@epitech.eu>
2026-02-16 18:55:52 +0100

    sync: 2026-02-16 18:55:52

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-200-LYN-2-1-bsminishell1-6
+** File description:
+** main.c
+*/
+
+#include "my.h"
+
+int main(int ac, char **av, char **env)
+{
+    shell_t shell = {NULL, NULL, 0, 0, NULL};
+
+    if (!get_env(env, &shell))
+        return 84;
+    handle_sh(&shell);
+    free_list(shell.env);
+    return 0;
+}
---
+++ src/utils/my_strncmp.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** G-CPE-100-LYN-1-1-cpoolday06-7
+** File description:
+** my_strncmp.c
+*/
+
+#include "my.h"
+
+int my_strncmp(char const *s1, char const *s2, int n)
+{
+    if (my_strlen(s1) < my_strlen(s2) && n > my_strlen(s2))
+        return -1;
+    else if (my_strlen(s1) > my_strlen(s2) && n > my_strlen(s1))
+        return 1;
+    for (int i = 0; i < n; i++) {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+    }
+    return 0;
+}
