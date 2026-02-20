1740a0662c53348f5f6b67cc15aafcb17cf52e29
Enzo Villalba <enzo.villalba@epitech.eu>
2026-02-20 03:27:36 +0100

    clean before sync

--- src/types/castv2.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'castv2-client' {
-    export class Client {
-        connect(ip: string, callback: () => void): void;
-        launch(receiver: any, callback: (err: Error | null, player: any) => void): void;
-        close(): void;
-        on(event: string, callback: (err: Error) => void): void;
-    }
-    export const DefaultMediaReceiver: any;
-}
