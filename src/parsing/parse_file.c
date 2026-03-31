504544e94af6734ef4470a9eadb843561c4cca30
Enzo Villalba <enzo.villalba@epitech.eu>
2026-03-31 02:17:38 +0200

    sync: 2026-03-31 02:17:38

---
+++ src/parsing/parse_file.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2026
+** robotfactory [WSL: Ubuntu]
+** File description:
+** parse_file
+*/
+
+#include "op.h"
+
+data_t *pars_file(const char *filename, data_t *data)
+{
+    data_t *temp = malloc(sizeof(data_t));
+    FILE *fstream = fopen(filename, "r");
+
+    if (!temp) {
+        if (fstream)
+            fclose(fstream);
+        return NULL;
+    }
+    temp->header = parse_header(fstream);
+    if (!temp->header)
+        return free_return(temp);
+    temp->filename = get_final_name(filename);
+    if (!temp->filename) {
+        return free_return(temp);
+    }
+    parse_body(fstream, data);
+    return temp;
+}
+/* --ICI-- */
