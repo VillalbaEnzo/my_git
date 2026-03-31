93420af3063f78110c74f17dc42fb7aa09528493
Enzo Villalba <enzo.villalba@epitech.eu>
2026-03-31 13:56:52 +0200

    sync: 2026-03-31 13:56:52

---
+++ src/handle/handle_pipe.c
@@ -0,0 +1,61 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-200-LYN-2-1-minishell2-7 [WSL: Ubuntu-24.04]
+** File description:
+** handle_pipe.c
+*/
+
+#include "my.h"
+
+struct contexte_s {
+    char **piped_cmd;
+    int fd[2];
+    int prev_fd;
+    int i;
+    char **cmds;
+};
+
+static void children_exec(struct contexte_s *contexte, shell_t *shell)
+{
+    char *spaced_cmd = NULL;
+
+    if (contexte->prev_fd != 0) {
+        dup2(contexte->prev_fd, 0);
+        close(contexte->prev_fd);
+    }
+    if (contexte->piped_cmd[contexte->i + 1]) {
+        dup2(contexte->fd[1], 1);
+        close(contexte->fd[1]);
+        close(contexte->fd[0]);
+    }
+    spaced_cmd = handle_add_spaces(contexte->piped_cmd[contexte->i]);
+    contexte->cmds = split(spaced_cmd, " \t");
+    shell->env_cp = chained_list_to_buf(shell->env);
+    handle_path(shell, contexte->cmds);
+    execve(contexte->cmds[0], contexte->cmds, shell->env_cp);
+    exit(1);
+    free(spaced_cmd);
+}
+
+void exec_pipeline(char **piped_cmd, shell_t *shell)
+{
+    struct contexte_s contexte;
+    int status;
+
+    contexte.piped_cmd = piped_cmd;
+    contexte.prev_fd = 0;
+    for (contexte.i = 0; piped_cmd[contexte.i]; contexte.i++) {
+        if (piped_cmd[contexte.i + 1])
+            pipe(contexte.fd);
+        if (fork() == 0) {
+            children_exec(&contexte, shell);
+        }
+        if (contexte.prev_fd != 0)
+            close(contexte.prev_fd);
+        if (piped_cmd[contexte.i + 1]) {
+            close(contexte.fd[1]);
+            contexte.prev_fd = contexte.fd[0];
+        }
+    }
+    while (waitpid(-1, &status, 0) > 0);
+}
